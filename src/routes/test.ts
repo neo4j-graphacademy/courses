24f325e81e2c8f8d3d377aa440646837ddeab870
adam-cowley <github@adamcowley.co.uk>
2021-05-27 09:37:30 +0100

    Better loading state, sandbox test routes

--- src/routes/test.ts
+++ src/routes/test.ts
@@ -1,12 +1,23 @@
 import { Router } from 'express'
+import { devSandbox } from '../domain/model/sandbox.mocks'
 import { write } from '../modules/neo4j'
 
 const router = Router()
 
-router.get('/', async (req, res, next) => {
+router.get('/', async (req, res) => {
     await write(`MATCH (e:Enrolment) DETACH DELETE e`)
 
     res.redirect('/logout')
+})
+
+router.get('/sandbox/SandboxGetRunningInstancesForUser', (req, res) => {
+    res.json([
+        devSandbox()
+    ])
+})
+
+router.get('/sandbox/SandboxRunInstance', (req, res) => {
+    res.json(devSandbox())
 })
 
 export default router
