a406b67609a1971de1e9a7ed7fb33588b9a05b2a
adam-cowley <adam@adamcowley.co.uk>
2021-12-01 19:00:56 +0000

    Refactor code to always use createAndSave and prefer API to DB

--- src/domain/services/create-and-save-sandbox.ts
+++ src/domain/services/create-and-save-sandbox.ts
@@ -1,10 +1,23 @@
+import { Transaction } from "neo4j-driver";
 import { write } from "../../modules/neo4j";
 import { createSandbox, Sandbox } from "../../modules/sandbox";
+import { CourseWithProgress } from "../model/course";
 
-export async function createAndSaveSandbox(token: string, usecase: string, enrolmentId: string): Promise<Sandbox> {
+export async function createAndSaveSandbox(token: string, course: CourseWithProgress, tx?: Transaction): Promise<Sandbox | undefined> {
+    if ( !course.usecase ) {
+        return
+    }
+
+    // Prefer the DB record?
+    if ( process.env.SANDBOX_PREFER_EXISTING && course.sandbox ) {
+        return course.sandbox
+    }
+
+    const { enrolmentId, usecase, } = course
+
     const sandboxOutput = await createSandbox(token, usecase)
 
-    const res = await write(`
+    const query = `
         MATCH (e:Enrolment {id: $id})
         MERGE (s:Sandbox {id: toInteger($sandbox.id)})
         SET s += $sandbox,
@@ -12,18 +25,27 @@ export async function createAndSaveSandbox(token: string, usecase: string, enrol
                 s.expiresAt = datetime({epochMillis: toInteger($sandbox.expires)})
         MERGE (e)-[:HAS_SANDBOX]->(s)
         RETURN s { .* } AS sandbox
-    `, { id: enrolmentId, sandbox: {
-        id: sandboxOutput.sandboxId,
-        hashKey: sandboxOutput.sandboxHashKey,
-        host: sandboxOutput.host,
-        ip: sandboxOutput.ip,
-        boltPort: sandboxOutput.boltPort,
-        expires: sandboxOutput.expires,
-        usecase: sandboxOutput.usecase,
-        username: sandboxOutput.username,
-        password: sandboxOutput.password,
-        scheme: sandboxOutput.scheme,
-    } })
+    `
+
+    const params = {
+        id: enrolmentId,
+        sandbox: {
+            id: sandboxOutput.sandboxId,
+            hashKey: sandboxOutput.sandboxHashKey,
+            host: sandboxOutput.host,
+            ip: sandboxOutput.ip,
+            boltPort: sandboxOutput.boltPort,
+            expires: sandboxOutput.expires,
+            usecase: sandboxOutput.usecase,
+            username: sandboxOutput.username,
+            password: sandboxOutput.password,
+            scheme: sandboxOutput.scheme,
+        }
+    }
+
+    const res = tx !== undefined
+        ? await tx.run(query, params)
+        : await write(query, params)
 
     return res.records[0].get('sandbox')
 }
