e87f53cbf169f3aa71bcc827c789120e3e5b50b2
adam-cowley <adam@adamcowley.co.uk>
2022-04-08 07:43:05 +0100

    Lint, generate images

--- src/commands/generate-images.ts
+++ src/commands/generate-images.ts
@@ -57,6 +57,7 @@ const renderCourses = async () => {
             }
         }
         catch(e) {
+            // tslint:disable-next-line no-console
             console.error(e)
         }
     }
