18859c458f7b6834e7f57199798a375b26fde6aa
adam-cowley <adam@adamcowley.co.uk>
2022-04-27 12:07:59 +0100

    Add individual tests

---
+++ cypress/integration/courses/modeling-fundamentals.spec.js
@@ -0,0 +1,12 @@
+describe('Graph Data Modeling Fundamentals', () => {
+    it('should pass modeling-fundamentals', () => {
+        cy.setup()
+        cy.login()
+
+        cy.getCourseDetails('modeling-fundamentals')
+            .then(([course]) => {
+                cy.log(course)
+                cy.enrol(course)
+            })
+    })
+})
