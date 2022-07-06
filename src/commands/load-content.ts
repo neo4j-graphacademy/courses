059f89dcbe82c51f6211a9e0b4969d27e461a9e9
adam-cowley <adam@adamcowley.co.uk>
2022-07-06 16:16:11 +0100

    Add timing to load-content command

--- src/commands/load-content.ts
+++ src/commands/load-content.ts
@@ -22,7 +22,14 @@ if ( !NEO4J_HOST || !NEO4J_USERNAME || !NEO4J_PASSWORD ) {
     throw new Error(`neo4j credentials not defined`)
 }
 
+const start = Date.now()
+
 initNeo4j(NEO4J_HOST as string, NEO4J_USERNAME as string, NEO4J_PASSWORD as string)
     .then(() => mergeCourses())
     .then(() => mergeCategories())
     .then(() => close())
+    .then(() => {
+        const end = Date.now()
+
+        console.log(`⌚️ Completed in ${end-start}ms`)
+    })
