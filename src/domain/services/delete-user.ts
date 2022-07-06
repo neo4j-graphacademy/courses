08772f7d5a30a22125d566b6434a18e7a76b0964
adam-cowley <adam@adamcowley.co.uk>
2022-07-06 16:11:54 +0100

    Remove :previous: asciidoc attribute

--- src/domain/services/delete-user.ts
+++ src/domain/services/delete-user.ts
@@ -6,10 +6,8 @@ export async function deleteUser(user: User): Promise<boolean> {
     const res = await write(`
         MATCH (u:User {sub: $id})
         SET u:DeletedUser,
-            u = { sub: '[deleted]-'+ apoc.text.base64Decode(u.sub), createdAt: u.createdAt, deletedAt: timestamp() }
-
-        // MATCH p = (u)-[:HAS_ENROLMENT|HAS_ATTEMPT|PROVIDED_ANSWER*1..3]->(n)
-        // DETACH DELETE n, u
+            u.sub = '[deleted]-'+ apoc.text.base64Encode(u.sub),
+            u.deletedAt = timestamp()
 
         RETURN true AS status
     `, { id: user.sub })
