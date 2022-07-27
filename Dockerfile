2605882580cd3fb30b2f86363e400fd4be9874cc
Adam Cowley <adam@adamcowley.co.uk>
2022-07-27 17:28:40 +0100

    Sync asciidoc from content repo, don't block thread when caching HTML

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,17 @@
 FROM node:16-alpine
 WORKDIR /app
-COPY package*.json /app/
+
+RUN apk add jq curl
+
+COPY . /app/
+
 RUN NODE_ENV=production npm install
 ENV NODE_ENV production
-COPY . /app/
+
+RUN chmod +x /app/sync
+RUN /app/sync
+
 RUN npm run build
 
 EXPOSE 3000
 CMD ["node", "--max-http-header-size=24000", "/app/dist/main.js"]
-
-
--- src/domain/services/asciidoc/cache-html.ts
+++ src/domain/services/asciidoc/cache-html.ts
@@ -5,11 +5,9 @@ import { getPageAttributes } from "../../../utils";
 import { STATUS_ACTIVE } from "../../model/course";
 
 export async function cacheHTML(): Promise<void> {
-    if ( ! ASCIIDOC_CACHING_ENABLED ) {
+    if (!ASCIIDOC_CACHING_ENABLED) {
         return
     }
-
-    const start = new Date()
 
     const res = await read(`
         MATCH (c:Course)-[:HAS_MODULE]->(m)-[:HAS_LESSON]->(l)
@@ -17,8 +15,7 @@ export async function cacheHTML(): Promise<void> {
         RETURN c AS course, m.slug AS module, l.slug AS lesson
     `, { active: STATUS_ACTIVE })
 
-
-    for (const row of res.records) {
+    res.records.forEach(async record => {
         const { course, module, lesson } = row.toObject()
 
         // Generate Attributes for the page
@@ -32,15 +29,8 @@ export async function cacheHTML(): Promise<void> {
 
         // Cache it
         addToCache(key, html)
-    }
-
-    const end = new Date()
-
-    const interval = end.getTime() - start.getTime()
-    const difference = new Date(interval)
-
-    const lpad = (value: number): string => ('00'+value).substr(-2)
+    })
 
     /* tslint:disable-next-line */
-    console.log(`🧠 Cached ${res.records.length} lessons in ${lpad(difference.getMinutes())}:${lpad(difference.getSeconds())}.${difference.getMilliseconds()}`)
+    console.log(`🧠 Caching ${res.records.length} lessons`)
 }
