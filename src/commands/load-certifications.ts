ff527eba36c1a0b3741b748faeb0f5446917e914
adam-cowley <adam@adamcowley.co.uk>
2022-08-10 13:40:38 +0100

    Don't limit load-certifications, import in batches. Remove asciidoc snippets

--- src/commands/load-certifications.ts
+++ src/commands/load-certifications.ts
@@ -1,5 +1,5 @@
 import { Session, Transaction, Record } from 'neo4j-driver'
-import { createDriver } from "../modules/neo4j"
+import { createDriver } from '../modules/neo4j'
 
 import {
     NEO4J_HOST,
@@ -11,7 +11,9 @@ import {
 } from '../constants'
 
 const getLastCertification = async (session: Session): Promise<string> => {
-    const res = await session.readTransaction((tx: Transaction) => tx.run(`MATCH (c:FromCommunityGraph) RETURN max(c.completedAt) AS date`))
+    const res = await session.readTransaction((tx: Transaction) =>
+        tx.run(`MATCH (c:FromCommunityGraph) RETURN max(c.completedAt) AS date`)
+    )
     const [first] = res.records
     const value = first.get('date')
 
@@ -19,32 +21,34 @@ const getLastCertification = async (session: Session): Promise<string> => {
 }
 
 const main = async () => {
-    [
+    ;[
         'NEO4J_HOST',
         'NEO4J_USERNAME',
         'NEO4J_PASSWORD',
         'COMMUNITY_GRAPH_HOST',
         'COMMUNITY_GRAPH_USERNAME',
         'COMMUNITY_GRAPH_PASSWORD',
-    ].forEach(key => {
+    ].forEach((key) => {
         if (process.env[key] === undefined) {
             throw new Error(`Missing key ${key}`)
         }
     })
 
-    console.log(`Connecting to GraphAcademy at ${NEO4J_HOST}`);
+    console.log(`Connecting to GraphAcademy at ${NEO4J_HOST}`)
     const ga = await createDriver(NEO4J_HOST, NEO4J_USERNAME, NEO4J_PASSWORD)
     const gaSession = ga.session()
 
-    console.log(`Connecting to Community Graph at ${COMMUNITY_GRAPH_HOST}`);
+    console.log(`Connecting to Community Graph at ${COMMUNITY_GRAPH_HOST}`)
     const community = await createDriver(COMMUNITY_GRAPH_HOST, COMMUNITY_GRAPH_USERNAME, COMMUNITY_GRAPH_PASSWORD)
     const communitySession = community.session()
 
     const lastCertification = await getLastCertification(gaSession)
 
-    console.log(`\n🔎 Getting certifications since ${lastCertification}`);
+    console.log(`\n🔎 Getting certifications since ${lastCertification}`)
 
-    const readRes = await communitySession.readTransaction((tx: Transaction) => tx.run(`
+    const readRes = await communitySession.readTransaction((tx: Transaction) =>
+        tx.run(
+            `
         MATCH (c:Certification)<-[:TOOK]-(u)
         WHERE c.name IN [
             "neo4-3.x-certification-test",
@@ -68,29 +72,44 @@ const main = async () => {
             percentage: c.percent,
             completedAt: datetime({epochSeconds: c.finished})
         } AS certification
-    `, { lastCertification }))
+    `,
+            { lastCertification }
+        )
+    )
 
     const rows = readRes.records.map((row: Record) => row.toObject())
 
-    const writeRes = await gaSession.writeTransaction((tx: Transaction) => tx.run(`
-        UNWIND $rows AS row
-        MATCH (c:Course {slug: row.slug})
+    while (rows.length) {
+        const next = rows.splice(0, 1000)
+        console.log(next)
 
-        MERGE (u:User {sub: row.sub})
-        ON CREATE SET u.id = randomUuid(),
-            u.createdAt = datetime()
+        const writeRes = await gaSession.writeTransaction((tx: Transaction) =>
+            tx.run(
+                `
+            UNWIND $rows AS row
+            MATCH (c:Course {slug: row.slug})
 
-        MERGE (e:Enrolment {id: apoc.text.base64Encode(row.slug +'--'+ row.sub)})
-        ON CREATE SET e.createdAt = datetime()
-        SET e:CompletedEnrolment, e:FromCommunityGraph,
-            e += row.certification,
-            e.certificateNumber = toInteger(row.certificateNumber)
+            MERGE (u:User {sub: row.sub})
+            ON CREATE SET u.id = randomUuid(),
+                u.createdAt = datetime()
 
-        MERGE (u)-[:HAS_ENROLMENT]->(e)
-        MERGE (e)-[:FOR_COURSE]->(c)
-    `, { rows }))
+            MERGE (e:Enrolment {id: apoc.text.base64Encode(row.slug +'--'+ row.sub)})
+            ON CREATE SET e.createdAt = datetime()
+            SET e:CompletedEnrolment, e:FromCommunityGraph,
+                e += row.certification,
+                e.certificateNumber = toInteger(row.certificateNumber)
 
-    console.log(`🥇 Created ${writeRes.summary.counters.updates().nodesCreated} certificate nodes`)
+            MERGE (u)-[:HAS_ENROLMENT]->(e)
+            MERGE (e)-[:FOR_COURSE]->(c)
+        `,
+                { rows: next }
+            )
+        )
+
+        console.log(
+            `🥇 Created ${writeRes.summary.counters.updates().nodesCreated} certificate nodes. ${rows.length} left.`
+        )
+    }
 
     await ga.close()
     await community.close()
