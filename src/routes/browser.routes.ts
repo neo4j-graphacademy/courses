48e254a9ff5c27a3d33d89c27a42df7af5930756
Adam Cowley <adam@adamcowley.co.uk>
2022-09-05 10:48:34 +0100

    Force trailing slash on browser route

--- src/routes/browser.routes.ts
+++ src/routes/browser.routes.ts
@@ -4,12 +4,13 @@ import { Request, Response, Router, static as serveStatic } from 'express'
 import { requiresAuth } from 'express-openid-connect'
 import { getUser } from '../middleware/auth.middleware'
 import { User } from '../domain/model/user'
+import { forceTrailingSlash } from '../middleware/trailing-slash.middleware'
 
 const router = Router()
 
 const browserDist = path.join(__dirname, '..', '..', 'browser', 'dist')
 
-router.get('/', requiresAuth(), async (req: Request, res: Response) => {
+router.get('/', requiresAuth(), forceTrailingSlash, async (req: Request, res: Response) => {
     const user = await getUser(req) as User
     const html = fs.readFileSync(path.join(browserDist, 'index.html')).toString()
 
