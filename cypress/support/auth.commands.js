4d6cae112efc70b81b11aee09025328e59ddcdae
Adam Cowley <adam@adamcowley.co.uk>
2022-10-20 08:56:16 +0100

    Move cypress tests to courses repo

---
+++ cypress/support/auth.commands.js
@@ -0,0 +1,37 @@
+Cypress.Commands.add('setup', () => {
+    cy.visit(Cypress.env('setup_url') +'?email='+ encodeURIComponent(Cypress.env('user_email')))
+})
+
+Cypress.Commands.add('login', () => {
+    cy.intercept({ method: 'POST', url: '/callback' }).as('callback')
+        cy.intercept({ method: 'POST', url: '/courses/*/*/*' }).as('answer')
+        cy.intercept({ method: 'POST', url: '/courses/*/*/*/verify' }).as('verify')
+
+        cy.get('.navbar-login')
+            .click()
+
+        // Log in to Auth0
+        cy.get('#username').type(Cypress.env('user_email'))
+        cy.get('#password').type(Cypress.env('user_password'))
+        cy.get('button').contains('Login').click()
+
+        // Hack: Intercept and set cookie
+        cy.wait('@callback').should(({ response }) => {
+            const cookie = response.headers['set-cookie'].find(cookie => cookie.startsWith('appSession'));
+
+            const [name, payload] = cookie.split('=')
+            const [value] = payload.split(';')
+
+            cy.setCookie(name, value)
+
+            cy.reload()
+        })
+
+        // The user should now be logged in
+        cy.get('.navbar-account')
+            .contains('My Account')
+})
+
+Cypress.Commands.add('logout', () => {
+    cy.visit('/logout')
+})
