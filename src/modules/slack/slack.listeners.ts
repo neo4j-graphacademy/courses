6066a46181358bc31d01cab00afd05cdc25d6967
Adam Cowley <adam@adamcowley.co.uk>
2022-12-07 10:58:22 +0000

    Remove order created and order shipped notifications in slack

--- src/modules/slack/slack.listeners.ts
+++ src/modules/slack/slack.listeners.ts
@@ -1,20 +1,17 @@
 import { emitter } from "../../events"
-import { OrderCreated } from "../printful/events/OrderCreated"
 import { OrderFailed } from "../printful/events/OrderFailed"
-import OrderShipped from "../printful/events/OrderShipped"
-import { notifyOrderCreated, notifyOrderFailed, notifyOrderShipped } from "./slack.module"
+import { notifyOrderFailed } from "./slack.module"
 
 export default function initSlackListeners(): void {
-    emitter.on<OrderCreated>(OrderCreated, async ({ user, reward, order }) => {
-        await notifyOrderCreated(user, reward, order)
-    })
+    // emitter.on<OrderCreated>(OrderCreated, async ({ user, reward, order }) => {
+    //     await notifyOrderCreated(user, reward, order)
+    // })
 
-    emitter.on<OrderShipped>(OrderShipped, async ({ order, shipment }) => {
-        await notifyOrderShipped(order, shipment)
-    })
+    // emitter.on<OrderShipped>(OrderShipped, async ({ order, shipment }) => {
+    //     await notifyOrderShipped(order, shipment)
+    // })
 
     emitter.on<OrderFailed>(OrderFailed, async ({ order, reason }) => {
         await notifyOrderFailed(order, reason)
     })
-
 }
