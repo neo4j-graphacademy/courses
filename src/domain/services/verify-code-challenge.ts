d2706f2de11416f8c36daea8fbf9310e64e3f85a
Adam Cowley <adam@adamcowley.co.uk>
2023-02-22 07:03:02 +0000

    Store verify results regardless of condition

--- src/domain/services/verify-code-challenge.ts
+++ src/domain/services/verify-code-challenge.ts
@@ -8,6 +8,14 @@ import { User } from "../model/user";
 import { getCourseWithProgress } from "./get-course-with-progress";
 import { saveLessonProgress } from "./save-lesson-progress";
 
+function createAnswer(reason): Answer[] {
+    return [{
+        id: 'verify',
+        correct: false,
+        answers: null,
+        reason,
+    }]
+}
 
 
 export async function verifyCodeChallenge(user: User, token: string, course: string, module: string, lesson: string): Promise<LessonWithProgress | false> {
@@ -15,52 +23,52 @@ export async function verifyCodeChallenge(user: User, token: string, course: str
     const { usecase } = progress
     const verify = await getLessonCypherFile(course, module, lesson, 'verify')
 
-    // No usecase or verify?
-    if (usecase === undefined || verify === undefined) {
-        return false
-    }
-
     // No sandbox? Return false
-    const sandbox = await getSandboxForUseCase(token, user, usecase)
+    const sandbox = usecase ? await getSandboxForUseCase(token, user, usecase) : undefined
 
-    if (!sandbox) {
-        return false
+    // Answer array
+    let answers: Answer[] = createAnswer('Internal Server Error: unknown')
+
+    // No usecase or verify?
+    if (usecase === undefined) {
+        answers = createAnswer(`Internal Server Error: Could not find usecase`)
     }
+    else if (verify === undefined) {
+        answers = createAnswer(`Internal Server Error: Could not find verification Cypher`)
+    }
+    else if (!sandbox) {
+        answers = createAnswer(`Internal Server Error: Could not find sandbox for usecase ${usecase}`)
+    }
+    else {
+        const host = `bolt://${sandbox.ip}:${sandbox.boltPort}`
+        const { username, password } = sandbox
 
-    const host = `bolt://${sandbox.ip}:${sandbox.boltPort}`
-    const { username, password } = sandbox
+        const driver = await createDriver(host, username, password)
+        const session = driver.session()
 
-    const driver = await createDriver(host, username, password)
+        try {
+            const res = await session.executeRead(tx => tx.run(verify))
 
-    const session = driver.session()
-
-    let answers: Answer[] = []
-
-    try {
-        const res = await session.executeRead(tx => tx.run(verify))
-
-        // If no records are returned then the test has failed
-        if (res.records.length > 0) {
-            answers = res.records.map(row => ({
-                id: row.has('task') ? row.get('task') : 'verify',
-                correct: row.get('outcome'),
-                answers: row.has('answers') ? row.get('answers') as string[] : null,
-                reason: row.has('reason') ? row.get('reason') : null,
-            }))
+            // If no records are returned then the test has failed
+            if (res.records.length > 0) {
+                answers = res.records.map(row => ({
+                    id: row.has('task') ? row.get('task') : 'verify',
+                    correct: row.get('outcome'),
+                    answers: row.has('answers') ? row.get('answers') as string[] : null,
+                    reason: row.has('reason') ? row.get('reason') : null,
+                }))
+            }
+            else {
+                answers = createAnswer(`No rows returned by verification Cypher`)
+            }
         }
-    }
-    catch (e: any) {
-        notify(e, error => {
-            error.setUser(user.sub, user.email, user.name)
-        })
+        catch (e: any) {
+            notify(e, error => {
+                error.setUser(user.sub, user.email, user.name)
+            })
 
-        answers = [{
-            id: 'verify',
-            correct: false,
-            answers: null,
-            reason: `Internal Server Error: ${e.message}`,
-        }]
-
+            answers = createAnswer(`Internal Server Error: ${e.message}`)
+        }
     }
 
     // Save outcome
