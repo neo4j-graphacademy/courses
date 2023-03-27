07ad647fd9c4ff035451ee4fab5d184755c35ee5
Adam Cowley <adam@adamcowley.co.uk>
2023-03-27 16:23:26 +0100

    Remove addthis, redefine own

--- src/routes/certificate.routes.ts
+++ src/routes/certificate.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { BASE_URL } from "../constants";
 import getCertificateById from "../domain/services/get-certificate-by-id";
+import { getUser } from "../middleware/auth.middleware";
 import { getUserName } from "../utils";
 import { courseOgBadgeImage, courseOgBannerImage } from "./route.utils";
 
@@ -11,6 +12,7 @@ router.get('/:id', async (req, res) => {
     const { id } = req.params
 
     const { course, user } = await getCertificateById(id)
+    const currentUser = await getUser(req)
 
     const userName = getUserName(user)
 
@@ -25,7 +27,7 @@ router.get('/:id', async (req, res) => {
         title: [course.title, `${userName}'s Achievements`].join(' | '),
         course,
         name: userName,
-        own: false,
+        own: user.id === currentUser?.id,
         badge: courseOgBadgeImage(course.slug),
         ogTitle: `${userName} earned the ${course.title} badge on #Neo4j #GraphAcademy`,
         ogDescription: `On ${new Intl.DateTimeFormat('en-US', { dateStyle: 'medium' }).format(new Date(course.completedAt?.toString()))} ${userName} earned the ${course.title} badge.  Test yourself with #Neo4j #GraphAcademy...`,
