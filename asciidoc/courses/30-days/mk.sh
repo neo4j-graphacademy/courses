fbf58cae8c854fccf66ff2a303495291efdaf6f6
Adam Cowley <adam@adamcowley.co.uk>
2023-04-25 15:12:05 +0100

    Structure for 30-days

---
+++ asciidoc/courses/30-days/mk.sh
@@ -0,0 +1,86 @@
+mkdir modules/2-cypher/lessons/day-6-the-where-clause
+mkdir modules/2-cypher/lessons/day-7-the-create-statement
+mkdir modules/2-cypher/lessons/day-8-the-merge-statement
+mkdir modules/2-cypher/lessons/day-9-set-remove-properties
+mkdir modules/2-cypher/lessons/day-10-delete
+mkdir modules/2-data-modeling/lessons/day-11-nouns-verbs-and-adjectives
+mkdir modules/2-data-modeling/lessons/day-12-nodes-properties-or-labels
+mkdir modules/2-data-modeling/lessons/day-13-intermediate-nodes
+mkdir modules/2-data-modeling/lessons/day-14-refactoring-data
+mkdir modules/2-data-modeling/lessons/day-15-indexes-and-constraints
+mkdir modules/3-intermediate-cypher/lessons/day-16-load-csv
+mkdir modules/3-intermediate-cypher/lessons/day-17-the-with-clause
+mkdir modules/3-intermediate-cypher/lessons/day-18-aggregations
+mkdir modules/3-intermediate-cypher/lessons/day-19-unwind
+mkdir modules/3-intermediate-cypher/lessons/day-20-subqueries
+mkdir modules/4-advanced-cypher/lessons/day-21-list-comprehension
+mkdir modules/4-advanced-cypher/lessons/day-22-map-projection
+mkdir modules/4-advanced-cypher/lessons/day-23-subqueries
+mkdir modules/4-advanced-cypher/lessons/day-24-dates-times-and-spatial
+mkdir modules/4-advanced-cypher/lessons/day-25-introduction-to-apoc
+mkdir modules/5-practical-neo4j/lessons/day-26-introduction-to-apoc
+mkdir modules/5-practical-neo4j/lessons/day-27-building-an-application
+mkdir modules/5-practical-neo4j/lessons/day-28-graph-data-science-with-aurads
+mkdir modules/5-practical-neo4j/lessons/day-29-common-graph-data-science-use-cases
+mkdir modules/5-practical-neo4j/lessons/day-30-best-practices
+mkdir modules/6-next-steps/lessons/day-31-recap-and-next-steps
+mkdir modules/2-cypher/lessons/day-challenge-deleting-emil
+mkdir modules/2-data-modeling/lessons/day-challenge-importing-ratings
+mkdir modules/3-intermediate-cypher/lessons/day-challenge-query-optimisation
+mkdir modules/4-advanced-cypher/lessons/day-challenge-challenge
+mkdir modules/5-practical-neo4j/lessons/day-challenge-running-an-algorithm
+
+mkdir modules/2-cypher
+mkdir modules/2-cypher/lessons
+Echo '== Cypher' >> modules/2-cypher/module.adoc
+Echo '== The WHERE Clause' >> modules/2-cypher/lessons/day-6-the-where-clause/lesson.adoc
+Echo '== The CREATE Statement' >> modules/2-cypher/lessons/day-7-the-create-statement/lesson.adoc
+Echo '== The MERGE Statement' >> modules/2-cypher/lessons/day-8-the-merge-statement/lesson.adoc
+Echo '== SET & REMOVE Properties' >> modules/2-cypher/lessons/day-9-set-remove-properties/lesson.adoc
+Echo '== DELETE' >> modules/2-cypher/lessons/day-10-delete/lesson.adoc
+Echo '== Deleting Emil' >> modules/2-cypher/lessons/challenge-deleting-emil/lesson.adoc
+
+mkdir modules/2-data-modeling
+mkdir modules/2-data-modeling/lessons
+Echo '== Data Modeling' >> modules/2-data-modeling/module.adoc
+Echo '== Nouns, Verbs and Adjectives' >> modules/2-data-modeling/lessons/day-11-nouns-verbs-and-adjectives/lesson.adoc
+Echo '== Nodes, Properties or Labels' >> modules/2-data-modeling/lessons/day-12-nodes-properties-or-labels/lesson.adoc
+Echo '== Intermediate Nodes' >> modules/2-data-modeling/lessons/day-13-intermediate-nodes/lesson.adoc
+Echo '== Refactoring Data' >> modules/2-data-modeling/lessons/day-14-refactoring-data/lesson.adoc
+Echo '== Indexes and Constraints' >> modules/2-data-modeling/lessons/day-15-indexes-and-constraints/lesson.adoc
+Echo '== Importing Ratings' >> modules/2-data-modeling/lessons/challenge-importing-ratings/lesson.adoc
+
+mkdir modules/3-intermediate-cypher
+mkdir modules/3-intermediate-cypher/lessons
+Echo '== Intermediate Cypher' >> modules/3-intermediate-cypher/module.adoc
+Echo '== LOAD CSV' >> modules/3-intermediate-cypher/lessons/day-16-load-csv/lesson.adoc
+Echo '== The WITH Clause' >> modules/3-/lessons/day-17-the-with-clause/lesson.adoc
+Echo '== Aggregations' >> modules/3-intermediate-cypher/lessons/day-18-aggregations/lesson.adoc
+Echo '== UNWIND' >> modules/3-intermediate-cypher/lessons/day-19-unwind/lesson.adoc
+Echo '== Subqueries' >> modules/3-intermediate-cypher/lessons/day-20-subqueries/lesson.adoc
+Echo '== Query Optimisation' >> modules/3-intermediate-cypher/lessons/challenge-query-optimisation/lesson.adoc
+
+mkdir modules/4-advanced-cypher
+mkdir modules/4-advanced-cypher/lessons
+Echo '== Advanced Cypher' >> modules/4-advanced-cypher/module.adoc
+Echo '== Subqueries' >> modules/4-advanced-cypher/lessons/day-21-subqueries/lesson.adoc
+Echo '== Map Projection' >> modules/4-advanced-cypher/lessons/day-22-map-projection/lesson.adoc
+Echo '== Subqueries' >> modules/4-advanced-cypher/lessons/day-23-subqueries/lesson.adoc
+Echo '== Dates, Times and Spatial' >> modules/4-advanced-cypher/lessons/day-24-dates-times-spatial/lesson.adoc
+Echo '== Introduction to APOC' >> modules/4-advanced-cypher/lessons/day-25-introduction-to-apoc/lesson.adoc
+Echo '== challenge' >> modules/4-advanced-cypher/lessons/challenge-challenge/lesson.adoc
+
+mkdir modules/5-practical-neo4j
+mkdir modules/5-practical-neo4j/lessons
+Echo '== Practical Neo4j' >> modules/5-practical-neo4j/module.adoc
+Echo '== Introduction to APOC' >> modules/5-practical-neo4j/lessons/day-26-introduction-to-apoc/lesson.adoc
+Echo '== Building an application' >> modules/5-practical-neo4j/lessons/day-27-building-an-application/lesson.adoc
+Echo '== Graph Data Science with AuraDS' >> modules/5-practical-neo4j/lessons/day-28-graph-data-science-with-aurads/lesson.adoc
+Echo '== Common Graph Data Science Use Cases' >> modules/5-practical-neo4j/lessons/day-29-common-graph-data-science-use-cases/lesson.adoc
+Echo '== Best Practices' >> modules/5-practical-neo4j/lessons/day-30-best-practices/lesson.adoc
+Echo '== Running an Algorithm' >> modules/5-practical-neo4j/lessons/challenge-running-an-algorithm/lesson.adoc
+
+mkdir modules/6-next-steps
+mkdir modules/6-next-steps/lessons
+Echo '== Next Steps' >> modules/6-next-steps/module.adoc
+Echo '== Recap and Next Steps' >> modules/6-next-steps/lessons/day-31-recap-and-next-steps/lesson.adoc
