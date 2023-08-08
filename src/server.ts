8da2e9450b2ac75f355accf403d84ba79c4c008a
Adam Cowley <adam@adamcowley.co.uk>
2023-08-08 15:57:39 +0100

    Redesign Banners (#142)
    * Add summary to emails, remove counts
    
    * Typos in graphql course
    
    * Hide summary for 2-neo4j-graphql
    
    * Update banner design
    
    * Add illustration to auto generation

---
+++ src/server.ts
@@ -0,0 +1,58 @@
+import express from 'express'
+import initNeo4j, { read } from './modules/neo4j'
+import { loadFile } from './modules/asciidoc'
+import { courseBadgePath, courseIllustrationPath, courseOverviewPath } from './utils'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
+const app = express()
+
+import {
+    NEO4J_HOST,
+    NEO4J_USERNAME,
+    NEO4J_PASSWORD,
+} from './constants'
+
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => res.redirect('/neo4j-fundamentals'))
+
+app.get('/:course', async (req, res) => {
+    await initNeo4j(NEO4J_HOST as string, NEO4J_USERNAME as string, NEO4J_PASSWORD as string)
+
+    const result = await read(`MATCH (c:Course {slug: $course}) RETURN c`, { course: req.params.course })
+    if (!result.records.length) {
+        return res.status(404).send('Not Found')
+    }
+    const [first] = result.records
+    const course = first.get('c').properties
+
+    const file = loadFile(courseOverviewPath(course.slug))
+
+    const css = readFileSync(join(__dirname, '..', 'resources', 'css', 'banner.css'))
+
+    let badge, illustration
+
+    try {
+        badge = readFileSync(courseBadgePath(course.slug)).toString()
+    }
+    catch (e: unknown) { }
+
+    try {
+        illustration = readFileSync(courseIllustrationPath(course.slug)).toString()
+    }
+    catch (e: unknown) { }
+
+    res.render('banner', {
+        overline: file.getAttribute('overline'),
+        title: file.getTitle(),
+        byline: file.getAttribute('caption'),
+        badge,
+        illustration,
+        css,
+    })
+
+})
+
+app.listen(3001, () => console.log('Listening on http://localhost:3001'))
