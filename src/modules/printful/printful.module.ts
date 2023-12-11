e3b00e5d5ee7b2f1e550ac613d3db09d9dbc0312
Adam Cowley <adam@adamcowley.co.uk>
2023-12-11 17:05:08 +0000

    Ooops

--- src/modules/printful/printful.module.ts
+++ src/modules/printful/printful.module.ts
@@ -152,10 +152,6 @@ export function formatRecipient(
             errors,
         )
     }
-    throw new ValidationError(
-        `Unable to validate address`,
-        { address1: 'NO good yo' },
-    )
 
     return recipient
 }
