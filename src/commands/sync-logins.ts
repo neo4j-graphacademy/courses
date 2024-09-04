2ee8c67a86dd15c9e8e21627f113b82d753a3455
Adam Cowley <adam@adamcowley.co.uk>
2024-09-04 08:22:47 +0100

    init analytics listeners

--- src/commands/sync-logins.ts
+++ src/commands/sync-logins.ts
@@ -6,29 +6,22 @@ import {
   NEO4J_PASSWORD
 } from '../constants'
 
-
 import { emitter } from "../events"
 import { initAnalytics } from "../modules/analytics/analytics.module"
-import initEmailListeners from "../listeners/emails"
-import { UserCompletedCourse } from "../domain/events/UserCompletedCourse"
-import { User } from "../domain/model/user"
-import { CourseWithProgress } from "../domain/model/course"
 import { UserLogin } from "../domain/events/UserLogin"
 import { JwtPayload } from "jsonwebtoken"
 import { Request } from "express"
-
-
+import initAnalyticsListeners from "../modules/analytics/analytics.listeners"
 
 const main = async () => {
   const driver = await initNeo4j(NEO4J_HOST, NEO4J_USERNAME, NEO4J_PASSWORD)
 
   initAnalytics()
-  await initEmailListeners()
+  await initAnalyticsListeners()
 
   const res = await write<{ user: JwtPayload, lastSeenAt: string }>(`
     MATCH (u:User)-[:HAS_ENROLMENT]->(e:Enrolment)
     WHERE datetime('2024-06-15') <= e.lastSeenAt <= datetime('2024-07-30')
-
     RETURN u { .* } AS user,
       max(e.lastSeenAt) AS lastSeenAt
   `)
@@ -42,7 +35,7 @@ const main = async () => {
     )
   }
 
-  console.log(`\n😩 ${res.records.length || 0} logins sent`)
+  console.log(`\n💨 ${res.records.length || 0} logins sent`)
 
   await driver.close()
 }
