d5a753f88c80b761c3464e58aba08dd919871169
Jennifer Reif <jennifer.reif@neo4j.com>
2024-10-25 15:44:02 -0500

    Jmhreif spring data (#268)
    Wrap up round1 editing for SDN course

--- asciidoc/courses/app-spring-data/modules/5-custom-cypher/lessons/3-c-custom-write/code/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-interface MovieRepository extends Neo4jRepository<Movie, String> {
-    //other methods
-
-    @Query("MERGE (m:Movie {movieId: $movie.__id__})" +
-            "SET m += $movie.__properties__, m.lastUpdated = datetime()" +
-            "RETURN m;")
-    Movie saveWithAudit(Movie movie);
-}
