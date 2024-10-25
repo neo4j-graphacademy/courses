b90672871e60c2612913f785fd11c5271242df5e
Jennifer Reif <jennifer.reif@neo4j.com>
2024-10-25 14:52:30 -0500

    Wrap up round1 editing for SDN course

--- asciidoc/courses/app-spring-data/modules/3-reading-data/lessons/6-create-role/sandbox.cypher
+++
@@ -1,2 +0,0 @@
-MATCH (p:Person)-[r:ACTED_IN]->(m:Movie {title: "Night at the Museum: Secret of the Tomb"})
-RETURN m.title, p.name, r.role;
--- asciidoc/courses/app-spring-data/modules/5-custom-cypher/lessons/3-c-custom-write/code/MovieControllerCompleted.java
+++ asciidoc/courses/app-spring-data/modules/5-custom-cypher/lessons/3-c-custom-write/code/MovieControllerCompleted.java
@@ -1,14 +1,3 @@
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.Optional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PutMapping;
-
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
@@ -43,7 +32,7 @@ public class MovieController {
     Iterable<Movie> findMoviesByPerson(@RequestParam String name) {
         return movieRepo.findMoviesByPerson(name);
     }
-
+    
     @PutMapping("/updateVotes")
     Movie updateVotes(@RequestParam String movieId) {
         return movieRepo.incrementImdbVotes(movieId);
