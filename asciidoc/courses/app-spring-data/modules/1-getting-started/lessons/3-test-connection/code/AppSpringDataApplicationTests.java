f60c7c13ae408c8c0fc77b5b7c5f549b61bf5eae
Jennifer Reif <jennifer.reif@neo4j.com>
2024-11-11 03:02:17 -0600

    Modify app-spring-data course with feedback (#277)
    * Testing spring data course
    
    * create lesson folders
    
    * Update pacing of course, test modules 1-3
    
    * Editing for modules 4-7
    
    * Update movie graph model image
    
    * Round of updates to modules 1-4
    
    * Round of updates to modules 1-4 (#262)
    
    Incorporating feedback on course structure
    
    * Wrap up round1 editing for SDN course
    
    * Jmhreif spring data (#268)
    
    Wrap up round1 editing for SDN course
    
    * Correct merge conflict issue
    
    * Correct conflict merge mishap (#269)
    
    * Cleanup merge conflict issue 2
    
    * Fix merge conflict issue 2 (#270)
    
    * SDN course: final cleanup
    
    * SDN course cleanup (#275)
    
    * categories
    
    * update gitpod overview
    
    * fix further reading
    
    * disable cache
    
    * minor grammar changes
    
    * run instructions
    
    * added code snippets
    
    * added imports
    
    * added imports
    
    * added code snippets
    
    * broke down task
    
    * broke down task
    
    * Incorporate feedback
    
    * Incorporate feedback (#276)
    
    * set to draft
    
    * added illustration + banner
    
    * Incorporate some more feedback
    
    * make course active
    
    * breakdown the activity
    
    * remove code tags
    
    ---------
    
    Co-authored-by: Adam Cowley <adam@adamcowley.co.uk>
    Co-authored-by: Martin O'Hanlon <martin.ohanlon@neo4j.com>

--- asciidoc/courses/app-spring-data/modules/1-getting-started/lessons/3-test-connection/code/AppSpringDataApplicationTests.java
+++ asciidoc/courses/app-spring-data/modules/1-getting-started/lessons/3-test-connection/code/AppSpringDataApplicationTests.java
@@ -1,20 +1,27 @@
+
+// tag::import[]
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.neo4j.driver.Driver;
 import org.springframework.beans.factory.annotation.Autowired;
+// end::import[]
 
 @SpringBootTest
 class AppSpringDataApplicationTests {
+    // tag::driver-injection[]
     // Driver injection
     final Driver driver;
+    // end::driver-injection[]
 
     public AppSpringDataApplicationTests(@Autowired Driver driver) {
         this.driver = driver;
     }
 
+    // tag::test-connection[]
     // Test connection
     @Test
     final void testConnection() {
         driver.verifyConnectivity();
     }
+    // end::test-connection[]
 }
--- asciidoc/courses/app-spring-data/modules/3-reading-data/lessons/1-map-movie/code/Movie.java
+++ asciidoc/courses/app-spring-data/modules/3-reading-data/lessons/1-map-movie/code/Movie.java
@@ -1,11 +1,14 @@
-// tag::movie[]
 package com.example.appspringdata;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 
+// tag::class[]
 @Node
 public class Movie {
+// end::class[]
+
+// tag::properties[]
     @Id
     private String movieId;
 
@@ -27,7 +30,9 @@ public class Movie {
 
     private String[] languages;
     private String[] countries;
+// end::properties[]
 
+// tag::constructor[]
     public Movie(String movieId, String title, String plot, String poster, String url, String imdbId, String tmdbId,
             String released, Long year, Long runtime, Long budget, Long revenue, Long imdbVotes, Double imdbRating,
             String[] languages, String[] countries) {
@@ -48,7 +53,9 @@ public class Movie {
         this.languages = languages;
         this.countries = countries;
     }
+// end::constructor[]
 
+// tag::getter_setter[]
     public String getMovieId() {
         return movieId;
     }
@@ -64,6 +71,7 @@ public class Movie {
     public void setTitle(String title) {
         this.title = title;
     }
+// end::getter_setter[]
 
     public String getPlot() {
         return plot;
@@ -177,4 +185,3 @@ public class Movie {
         this.countries = countries;
     }
 }
-// end::movie[]
