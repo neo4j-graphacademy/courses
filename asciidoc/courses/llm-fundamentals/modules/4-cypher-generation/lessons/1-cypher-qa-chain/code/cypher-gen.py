6feb0638be222f4e7574e86ca6d6b4f21ab0e008
Alex Thomas <alexthomas93@users.noreply.github.com>
2024-11-29 15:46:38 +0000

    Updated llm-fundamentals course

--- asciidoc/courses/llm-fundamentals/modules/4-cypher-generation/lessons/1-cypher-qa-chain/code/cypher-gen.py
+++ asciidoc/courses/llm-fundamentals/modules/4-cypher-generation/lessons/1-cypher-qa-chain/code/cypher-gen.py
@@ -1,6 +1,5 @@
 from langchain_openai import ChatOpenAI
-from langchain_community.graphs import Neo4jGraph
-from langchain.chains import GraphCypherQAChain
+from langchain_neo4j import GraphCypherQAChain, Neo4jGraph
 from langchain.prompts import PromptTemplate
 
 # tag::openai-neo4j[]
