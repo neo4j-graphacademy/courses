b11ab303b860feab95f0d4af9d1aa348de537c38
Adam Cowley <adam@adamcowley.co.uk>
2025-02-17 13:26:19 +0000

    maintenance 27 feb

--- src/routes/home.routes.ts
+++ src/routes/home.routes.ts
@@ -71,11 +71,11 @@ router.get('/', async (req, res, next) => {
             meta: [{
                 name: 'apple-itunes-app', content: 'app-id=1557747094'
             }],
-            // banner: {
-            //     text: 'Help us improve your experience on GraphAcademy  - ',
-            //     link: 'https://forms.gle/PbXHbFPYvBKvnqE59',
-            //     cta: 'Complete the survey',
-            // },
+            banner: {
+                text: 'Maintenance planned - 27 February from 10:00-12:00 UTC',
+                //     link: 'https://forms.gle/PbXHbFPYvBKvnqE59',
+                //     cta: 'Complete the survey',
+            },
             translate: translateEn,
         })
     }
