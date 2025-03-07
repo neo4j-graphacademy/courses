c3fbde6a01a8c6a176b206fcfe737c6f577432cb
Adam Cowley <adam@adamcowley.co.uk>
2025-03-07 14:38:10 +0000

    generate slides as pdf

--- src/commands/generate-slides.ts
+++ src/commands/generate-slides.ts
@@ -12,33 +12,38 @@ const main = async () => {
             const stats = statSync(join(COURSE_DIRECTORY, slug))
             return stats.isDirectory()
         })
-        // Check that summary.adoc exists in folder
+        // Check that slides should be generated
         .filter(slug => {
-            const path = join(COURSE_DIRECTORY, slug, 'summary.adoc')
-            const exists = existsSync(path)
-
-            const hasPdf = exists && readFileSync(path).toString().includes(':pdf-summary:')
+            const path = join(COURSE_DIRECTORY, slug, 'course.adoc')
+            const hasPdf = readFileSync(path).toString().includes(':slides: true')
 
             return hasPdf
         })
-        .filter(slug => !existsSync(join(COURSE_DIRECTORY, slug, 'summary.pdf')))
+        // Ignore if slides.pdf already exists
+        .filter(slug => !existsSync(join(COURSE_DIRECTORY, slug, 'slides.pdf')))
 
     const browser = await puppeteer.launch({ headless: 'shell' })
 
     for (const course of courses) {
         const page = await browser.newPage()
-        await page.goto(`http://localhost:3001/${course}/summary`, { waitUntil: 'networkidle0' })
-        const pdf = await page.pdf({ format: 'A4', landscape: true })
+        await page.goto(`http://localhost:3001/${course}/slides`, { waitUntil: 'networkidle0' })
+        const pdf = await page.pdf({
+            // format: 'A4', 
+            width: '680px',
+            height: '1220px',
+            landscape: true,
+            printBackground: true,
+        })
 
         // write pdf to file system
         await writeFileSync(
-            join(COURSE_DIRECTORY, course, 'summary.pdf'),
+            join(COURSE_DIRECTORY, course, 'slides.pdf'),
             pdf,
             {}
         )
     }
 
-    console.log(`📖 ${courses.length} Summary PDFs generated`)
+    console.log(`📖 ${courses.length} Slides PDFs generated`)
 }
 
 const server = app.listen(3001, async () => {
