0f0b4a7e2f571e58522b76511b8930e4d589e95c
Martin O'Hanlon <martin.ohanlon@neo4j.com>
2025-05-13 17:18:04 +0100

    added devcontainer config

---
+++ .devcontainer/post_attach.sh
@@ -0,0 +1,3 @@
+echo "The GraphAcademy courses devcontainer is ready!"
+echo "You can access the GraphAcademy app at http://localhost:3000"
+echo "Use 'npm run dev:watch' to start the content sync process"
