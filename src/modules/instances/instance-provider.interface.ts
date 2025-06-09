78ebdc01101953676c53a9b25ffdec4031cc5694
Adam Cowley <adam@adamcowley.co.uk>
2025-06-09 15:28:37 +0100

    remove old file, update instanceprovider executecypher def

--- src/modules/instances/instance-provider.interface.ts
+++ src/modules/instances/instance-provider.interface.ts
@@ -1,4 +1,4 @@
-import { EagerResult, QueryResult, RoutingControl } from "neo4j-driver";
+import { EagerResult, RoutingControl } from "neo4j-driver";
 import { Instance } from "../../domain/model/instance";
 import { User } from "../../domain/model/user";
 
@@ -9,5 +9,5 @@ export interface InstanceProvider {
   stopInstance(token: string, user: User, instanceHashKey: string): Promise<void>;
   createInstance(token: string, user: User, usecase: string, vectorOptimized: boolean, graphAnalyticsPlugin: boolean): Promise<Instance>;
   getOrCreateInstanceForUseCase(token: string, user: User, usecase: string, vectorOptimized: boolean, graphAnalyticsPlugin: boolean): Promise<Instance>;
-  async executeCypher<T = Record<string, any>>(token: string, user: User, usecase: string, cypher: string, params: Record<string, any>, routing: RoutingControl): Promise<EagerResult<T> | undefined> {
+  executeCypher<T = Record<string, any>>(token: string, user: User, usecase: string, cypher: string, params: Record<string, any>, routing: RoutingControl): Promise<EagerResult<T> | undefined>;
 }
