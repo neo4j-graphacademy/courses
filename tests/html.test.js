9f7abbf8e1a52c196d875682a1b52d9127ecc96b
Adam Cowley <adam.cowley@neo4j.com>
2025-12-11 14:57:08 +0000

    Detect broken headers in html build (#460)
    * detect broken headers in html build
    
    * test html before  db sync and release

--- tests/html.test.js
+++ tests/html.test.js
@@ -1,21 +1,51 @@
-const { readFileSync } = require("fs")
-const { globSync } = require("glob")
-const { globJoin } = require("./utils")
+const { readFileSync } = require("fs");
+const { globSync } = require("glob");
+const { globJoin } = require("./utils");
 
-describe('html generation', () => {
+describe("html generation", () => {
   const files = globSync(
-    globJoin(__dirname, '..', 'build', '**', '*.html')
-  )
+    globJoin(__dirname, "..", "build", "**", "gds-fundamentals", "**", "*.html")
+  );
 
   for (const file of files) {
     describe(file, () => {
-      it('should not have an unresolved directive', () => {
-        const buffer = readFileSync(file)
-        const text = buffer.toString()
+      it("should not have an unresolved directive", () => {
+        const buffer = readFileSync(file);
+        const text = buffer.toString();
 
-        expect(text).not.toContain('unresolved directive')
-      })
-    })
+        expect(text).not.toContain("unresolved directive");
+      });
+
+      it("should not have broken headers", () => {
+        const buffer = readFileSync(file);
+        const text = buffer.toString();
+
+        // Remove all <code>...</code> and <pre>...</pre> blocks
+        const CODE_BLOCK_REGEX = /<code[^>]*>[\s\S]*?<\/code>/gi;
+        const PRE_BLOCK_REGEX = /<pre[^>]*>[\s\S]*?<\/pre>/gi;
+        const textWithoutCode = text
+          .replace(CODE_BLOCK_REGEX, "")
+          .replace(PRE_BLOCK_REGEX, "");
+
+        // Find broken headers (=+ followed by space and text) anywhere in content
+        const BROKEN_HEADER_REGEX = /(?:^|>)(=+\s+[^<\n]+)/gm;
+        const brokenHeaders = [];
+        let match;
+        while ((match = BROKEN_HEADER_REGEX.exec(textWithoutCode)) !== null) {
+          const lineNumber = textWithoutCode
+            .substring(0, match.index)
+            .split("\n").length;
+          brokenHeaders.push({ line: lineNumber, text: match[1] });
+        }
+        if (brokenHeaders.length) {
+          const errorLines = brokenHeaders
+            .map(({ line, text }) => `Line ${line}: "${text.trim()}"`)
+            .join("\n");
+          throw new Error(
+            `Broken header lines found outside <code> blocks:\n${errorLines}`
+          );
+        }
+      });
+    });
   }
-
-})
+});
