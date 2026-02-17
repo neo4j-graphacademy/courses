b2480d53e2717e9254ee1f346ec358c8acdde00f
Adam Cowley <adam.cowley@neo4j.com>
2026-02-17 16:28:26 +0000

    open import tab links

--- src/modules/asciidoc/extensions/console-block-processor.extension.ts
+++ src/modules/asciidoc/extensions/console-block-processor.extension.ts
@@ -1,55 +1,57 @@
-import { CONSOLE_URL } from '../../../constants'
+import { CONSOLE_URL } from "../../../constants";
 
 /* eslint-disable */
 export function consoleBlockProcessor(registry: any) {
-    registry.blockMacro(function () {
-        // @ts-ignore
-        const self: any = this
+  registry.blockMacro(function () {
+    // @ts-ignore
+    const self: any = this;
 
-        self.named('console')
-        self.process(function (parent: any, text: any, attrs: any) {
-            // Replace backticks
-            text = text.replace(/`(.*)`/, '<code>$1</code>')
+    self.named("console");
+    self.process(function (parent: any, text: any, attrs: any) {
+      // Replace backticks
+      text = text.replace(/`(.*)`/, "<code>$1</code>");
 
-            const { connectUrl, guide, model, arg, cmd, tab, tool } = attrs
+      const { guide, model, arg, cmd, tab, tool } = attrs;
 
-            const url = new URL(CONSOLE_URL)
+      const url = new URL(CONSOLE_URL);
 
-            if (tab) {
-                url.pathname = `/${tab}`
-            }
+      if (tab) {
+        url.pathname = `/${tab}`;
+      }
 
-            if (tool) {
-                url.pathname = `/tools/${tool}`
-            }
+      if (tool) {
+        url.pathname = `/tools/${tool}`;
+      }
 
-            if (connectUrl) {
-                url.searchParams.set('connectURL', connectUrl)
-            }
+      const connectUrl = attrs["connect-url"];
 
-            if (guide) {
-                url.searchParams.set('cmd', 'guide')
-                url.searchParams.set('arg', guide)
-                url.searchParams.set('guide', guide)
-            }
+      if (connectUrl) {
+        url.searchParams.set("connectURL", connectUrl);
+      }
 
-            if (cmd) {
-                url.searchParams.set('cmd', cmd)
-            }
-            if (arg) {
-                url.searchParams.set('arg', arg)
-            }
+      if (guide) {
+        url.searchParams.set("cmd", "guide");
+        url.searchParams.set("arg", guide);
+        url.searchParams.set("guide", guide);
+      }
 
-            if (model) {
-                url.searchParams.set('model', model)
-            }
+      if (cmd) {
+        url.searchParams.set("cmd", cmd);
+      }
+      if (arg) {
+        url.searchParams.set("arg", arg);
+      }
 
-            // Always set attribution to graphacademy
-            url.searchParams.set('attribution', 'graphacademy')
+      if (model) {
+        url.searchParams.set("model", model);
+      }
 
-            const link = `<a class="btn btn-console" target="_blank" href="${url.toString()}">${text} &rarr;</a>`
+      // Always set attribution to graphacademy
+      url.searchParams.set("attribution", "graphacademy");
 
-            return self.createBlock(parent, 'pass', link)
-        })
-    })
+      const link = `<a class="btn btn-console" target="_blank" href="${url.toString()}">${text} &rarr;</a>`;
+
+      return self.createBlock(parent, "pass", link);
+    });
+  });
 }
